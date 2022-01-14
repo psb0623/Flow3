c7c9d4cdeca291149163e1fb84e38435ff941551
Zea <zeaear714@gmail.com>
2022-01-14 20:45:24 +0900

    Add sound Effecct

---
+++ src/audio/BackGroundSound.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Button, TextPropTypes } from 'react-native';
+import { Audio} from 'expo-av';
+import { Sound } from 'expo-av/build/Audio';
+
+const bgm = new Audio.Sound();
+
+export async function backgroundMusic(){
+
+    await Audio.setAudioModeAsync( {playsInSilentModeIOS : true});
+
+
+    try {
+        await bgm.loadAsync(require('../../assets/MainBGM.wav'));
+        await bgm.playAsync();
+    } catch (error) {
+        
+
+    }
+    bgm.setIsLoopingAsync(true);
+}
+
