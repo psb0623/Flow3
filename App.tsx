9db27f616df8acb2357d403c5242f75af467c227
moretall <seongho4996@naver.com>
2022-01-17 15:23:07 +0900

    mock server에 데이터 추가

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import {AppStack} from './src/page/stack/AppStack';
 import {useEffect, useState} from 'react';
 import {Splash} from './src/page/Splash/Splash';
-import {View} from 'react-native';
 import {backgroundMusic} from './src/audio/BackGroundSound';
 
 export default function App() {
